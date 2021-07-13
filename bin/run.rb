90548048f21b9445aaac43c6e6aaca0c3dbf9771
Joyce Lee <joycelee901@gmail.com>
2021-07-13 10:59:39 -0500

    moved models from /app to /lib; removed require_all app; update run.rb to remove duplicate require tty-prompt and add prompts; environment.rb: added SQL comment remover in a comment for future

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,20 @@
 require_relative '../config/environment'
-require "tty-prompt"
 
+prompt = TTY::Prompt.new
 
-puts "HELLO WORLD"
+# prompt.ask = ask a question
+# prompt.select = give user options to select // ('question?', ["option1", "option2"])
+# prompt.mask = prompt for password but masks what is typed in
+
+profile_choice = prompt.select('Are you a Job Seeker or Recruiter?', ["Job Seeker", "Recruiter"])
+
+if profile_choice == "Job Seeker"
+    username = prompt.ask("Name:")
+    # password = prompt.mask("Password:")
+
+    current_user = JobSeeker.all.find{|seeker| seeker.name == username}
+
+    puts "Hello #{current_user.name}!"
+end
+
+# puts "HELLO WORLD"
