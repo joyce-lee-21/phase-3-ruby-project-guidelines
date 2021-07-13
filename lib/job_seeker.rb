970399b0aa7e81f54c6c0843aebf17025279428a
Joyce Lee <joycelee901@gmail.com>
2021-07-13 15:15:15 -0500

    added aggregation methods to job_seeker

--- lib/job_seeker.rb
+++ lib/job_seeker.rb
@@ -8,6 +8,18 @@ class JobSeeker < ActiveRecord::Base
     def recruiters
         self.events.collect{|event| event.recruiter}
     end
+
+    def view_skills
+        self.skills.collect{|skills| skills.name}
+    end
+
+    def add_skill(name, level)
+        Skill.create(profile_id: self.profile.id, name: name, level: level)
+    end
+
+    def delete_skill(name)
+        self.skills.find_by(name: name).destroy
+    end
 end
 
 # below works:
@@ -16,6 +28,8 @@ end
 # JobSeeker.first.events.first.recruiter
 # JobSeeker.first.events
 # JobSeeker.first.add_events
+
+# match recruiter -> job seeker
 # JobSeeker.first.events.first.recruiter.profile.skills
 
 
